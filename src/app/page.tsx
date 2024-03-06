274eff4171e8aabd1e9c5e48a73797773888a5c4
wadile-hitesh <hiteshwadile.dev@gmail.com>
2024-03-06 18:59:28 +0530

    Navbar Started

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-center text-2xl">Hi, I'm Learning NextJS</h1>
+    </main>
+  );
+}
