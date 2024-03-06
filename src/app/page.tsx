873721b11554fb69b3d6204c31dfa8d1a2c03d41
wadile-hitesh <hiteshwadile.dev@gmail.com>
2024-03-06 20:36:12 +0530

    Sticky role Created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 export default function Home() {
@@ -6,6 +8,8 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       
       <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
     </main>
   );
 }
