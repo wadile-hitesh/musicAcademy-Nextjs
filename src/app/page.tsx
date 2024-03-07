e24add2b6b3148876c73d41955b9865d7eaad315
wadile-hitesh <hiteshwadile.dev@gmail.com>
2024-03-07 20:51:35 +0530

    Add testimonials section and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import MusicSchoolTestimonials from "@/components/TestimonialsCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 
@@ -10,6 +13,9 @@ export default function Home() {
       <HeroSection />
       <FeaturedCourses />
       <WhyChooseUs />
+      <MusicSchoolTestimonials />
+      <UpcomingWebinars />
+      <Instructors />
     </main>
   );
 }
